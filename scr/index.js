3397296b903ae3d8385c7b1a0912599bb4691efe
tocode <andrey.zarev@outlook.com>
2024-01-20 15:46:10 +0200

    set view folder

--- scr/index.js
+++ scr/index.js
@@ -1,1 +1,21 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const handlebars = require('express-handlebars');
+
+
+const port = 5000
+const app = express();
+
+app.engine("hbs", handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', "hbs");
+app.set('view', path.join(__dirname, 'src', 'views'));
+    
+
+app.get('/', (req, res) =>{
+app.router()
+});
+
+app.listen(port, ()=> console.log(`Server has been sucsessfully lauched on port ${port}`))
