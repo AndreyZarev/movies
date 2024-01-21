795507469e403d174bc03910131bff54b5827e3e
tocode <andrey.zarev@outlook.com>
2024-01-21 15:48:26 +0200

    fixed route link error

--- scr/index.js
+++ scr/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const handlebars = require('express-handlebars');
-
+const routes = require('./routes');
 
 const port = 5000
 const app = express();
@@ -12,10 +12,9 @@ app.engine("hbs", handlebars.engine({
 
 app.set('view engine', "hbs");
 app.set('views', path.join(__dirname, 'views'));
-    
+    // app.use(express.static(path.join(__dirname, "public")));
+    app.use(express.static(path.join(__dirname, "static")));
+app.use(routes)
 
-app.get('/', (req, res) =>{
-res.render("home", {layout: false});
-});
-app.use(express.static(path.join(__dirname, "static")));
+
 app.listen(port, ()=> console.log(`Server has been sucsessfully lauched on port ${port}`))
--- scr/routes.js
+++ scr/routes.js
@@ -1,1 +1,13 @@
 const router = require('express').Router();
+
+router.get('/', (req, res) =>{
+    res.render("home", {layout: false});
+    });
+
+router.get('/create', (req, res) =>{
+        res.render("create", {layout: false});
+        });
+    
+
+
+    module.exports = router
