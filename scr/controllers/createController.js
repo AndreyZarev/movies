65291c298d5703da4f5f159d7deddc0a2938c0dd
tocode <andrey.zarev@outlook.com>
2024-01-23 14:48:04 +0200

    Linked create section

--- scr/controllers/createController.js
+++ scr/controllers/createController.js
@@ -1,7 +1,14 @@
 const router = require('express').Router()
-
+const movieService = require('./../service/movieService')
 router.get('/movie/create', (req, res) => {
     res.render("create", { layout: false });
 });
+
+router.post('/movie/create', (req, res) => {
+   
+    movieService.create(req.body);
+    res.redirect('/')
+});
+
 
 module.exports = router
