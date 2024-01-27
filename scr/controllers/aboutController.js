039b7d71fe45c66abe009ba8fa54a3ba0de868fe
tocode <andrey.zarev@outlook.com>
2024-01-27 23:15:46 +0200

    linked details page

--- scr/controllers/aboutController.js
+++ scr/controllers/aboutController.js
@@ -4,3 +4,7 @@ router.get('/about', (req, res) => {
     res.render("about", { layout: false });
 });
 module.exports = router
+
+router.get('/movies/:movieId', (req, res) => {
+res.render("details", { layout: false });
+});
