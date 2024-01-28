b426a1f93cdb268e2d6c1b69240cc5fa57d70960
tocode <andrey.zarev@outlook.com>
2024-01-28 14:35:47 +0200

    hardcoded another movie

--- scr/service/movieService.js
+++ scr/service/movieService.js
@@ -1,14 +1,13 @@
 const movies = [
     {
         _id: 1,
-    title: 'fdsf',
-    genre: 'fdsfds',
-    director: 'xczxczx',
-    imageUrl: '.././img/jungle-cruise.jpeg',
-    year: '2323',
-    rating: '2',
-    description: 'wesdsdssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss'
-  
+        title: 'Home Alone',
+        genre: 'Genre: Comedy',
+        director: 'Sam Johvonen',
+        imageUrl: '.././img/home-alone.jpeg',
+        year: '2323',
+        rating: '5',
+        description: "It is Christmas time and the McCallister family is preparing for a vacation in Paris, France.But the youngest in the family, Kevin(Macaulay Culkin), got into a scuffle with his older brother Buzz(Devin Ratray) and was sent to his room, which is on the third floor of his house.Then, the next morning, while the rest of the family was in a rush to make it to the airport on time, they completely forgot about Kevin, who now has the house all to himself.Being home alone was fun for Kevin, having a pizza all to himself, jumping on his parents' bed, and making a mess. Then, Kevin discovers about two burglars, Harry(Joe Pesci) and Marv(Daniel Stern), about to rob his house on Christmas Eve.Kevin acts quickly by wiring his own house with makeshift booby traps to stop the burglars and to bring them to justice."
 }
 ];
 
@@ -16,23 +15,23 @@ exports.getAll = () => {
     return [...movies]
 }
 exports.create = (movieData) => {
-   movieData._id = Number(movies[movies.length - 1]._id) + 1;
+    movieData._id = Number(movies[movies.length - 1]._id) + 1;
     movies.push(movieData);
     console.log(movies);
 }
 
-exports.getOne = (movieId) => { 
+exports.getOne = (movieId) => {
 
-    let correctOne 
+    let correctOne
     // const movie = movies.find(movie => movies._id == movieId);
     const movie = movies.forEach(movie => {
-       
+
         if (movie._id == movieId) {
-           correctOne  = movie
+            correctOne = movie
 
         }
     });
-   
-return correctOne;
+
+    return correctOne;
 };
 
