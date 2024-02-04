b89abaca70f58321ae911001eab4bcb2a33d9f76
tocode <andrey.zarev@outlook.com>
2024-02-04 02:20:16 +0200

    added register to create in DB

--- scr/controllers/authController.js
+++ scr/controllers/authController.js
@@ -1,9 +1,17 @@
 const router = require('express').Router()
 
+const authService = require('../service/authService')
 
-
-router.get('/register', async (req, res) => {
+router.get('/register',  (req, res) => {
 res.render('auth/register')
 });
+
+
+router.post('/register', async (req, res) => {
+
+    const userData = req.body
+    const user = await authService.register(userData)
+//    const registerInfo = await 
+    });
 
 module.exports = router
