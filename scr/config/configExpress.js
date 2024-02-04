da3f71c7bef78ce6da6333bd7d046aa2fdd6bbe5
tocode <andrey.zarev@outlook.com>
2024-02-05 00:30:07 +0200

    add cookie parser

--- scr/config/configExpress.js
+++ scr/config/configExpress.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+
+const cookieParser = require('cookie-parser');
 function configExpress(app) {
     app.use(express.static(path.resolve('scr/static')));
     app.use(express.urlencoded({extended: false}));
