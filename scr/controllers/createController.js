105773c199f533db0b95e2b6458f7bba906f7738
tocode <andrey.zarev@outlook.com>
2024-02-10 00:43:42 +0200

    corrected catch in createController

--- scr/controllers/createController.js
+++ scr/controllers/createController.js
@@ -1,27 +1,26 @@
 const router = require('express').Router()
-
+const { getErrorMessage } = require('../utils/errorsHandler')
 const movieService = require('./../service/movieService')
-const {isAuth} = require('../middleware/authMiddleware')
+const { isAuth } = require('../middleware/authMiddleware')
 
 
 
-router.get('/movie/create',isAuth, (req, res) => {
+router.get('/movie/create', isAuth, (req, res) => {
     res.render("create");
 });
 
 router.post('/movie/create', async (req, res) => {
-   const newMovie = {
-    ...req.body,
-owner: req.user._id
-      }
-         try {
-    await movieService.create(newMovie);
-    res.redirect('/')
-   } catch (err) {
-    res.status(404)
-    console.log(err.message);
-    res.redirect('/movie/create')
-   }
+    const newMovie = {
+        ...req.body,
+        owner: req.user._id
+    }
+    try {
+        await movieService.create(newMovie);
+        res.redirect('/')
+    } catch (err) {
+        const message = getErrorMessage(err)
+        res.status(400).render('create', { error: message, ...newMovie })
+    }
 });
 
 
