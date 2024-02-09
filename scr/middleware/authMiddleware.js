3df4e0ddcb54e31d97ccef4192f500ad370dbf8a
tocode <andrey.zarev@outlook.com>
2024-02-10 00:42:25 +0200

    added from bracnch

--- scr/middleware/authMiddleware.js
+++ scr/middleware/authMiddleware.js
@@ -1,37 +1,37 @@
-const {SECRET} = require('../config/config')
+const { SECRET } = require('../config/config')
 const jwt = require('../lib/jwt')
 exports.auth = async (req, res, next) => {
 
-// works now
+    // works now
 
     const token = req.cookies["auth"];
 
     if (!token) {
-       
-return next()
+
+        return next()
     }
 
-try {
-    const decodedToken = await jwt.verify(token, SECRET)
+    try {
+        const decodedToken = await jwt.verify(token, SECRET)
 
-    req.user = decodedToken
-res.locals.isAuthenticated = true
+        req.user = decodedToken
+        res.locals.isAuthenticated = true
 
-    next()
-} catch  {
-res.clearCookie("auth")
-return res.redirect("/auth/login")
+        next()
+    } catch {
+        res.clearCookie("auth")
+        return res.redirect("/auth/login")
 
-}
+    }
 };
 
 
-exports.isAuth =  (req, res, next) => {
-   
+exports.isAuth = (req, res, next) => {
 
-    if(!req.user){
-      return  res.redirect("/auth/login")
+
+    if (!req.user) {
+        return res.redirect("/auth/login")
     }
-   
+
     next()
 }
