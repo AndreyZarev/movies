e13d3511ede4e1d79846c479ebdba4282a725b5c
tocode <andrey.zarev@outlook.com>
2024-02-10 00:46:06 +0200

    formatted auth service

--- scr/service/authService.js
+++ scr/service/authService.js
@@ -1,42 +1,45 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt');
 const jwt = require('../lib/jwt');
-const {SECRET} = require('../config/config');
+const { SECRET } = require('../config/config');
 const { email } = require('is');
 
 
 exports.register = (userData) => {
-    
-    const user = User.findOne({email: userData.email})
+
+    const user = User.findOne({ email: userData.email })
 
     if (user) {
-    throw new Error(`User ${user})already exists`);
+        throw new Error(`User already exists`);
     }
-    User.create(userData)};
+    User.create(userData)
+};
+
+
 
 exports.login = async (email, password) => {
-   
-    const user = await User.findOne({email})
-    
+
+    const user = await User.findOne({ email })
+
     if (!user) {
 
         throw new Error('Username or password,is incorrect')
 
     }
-    
+
     const isValid = await bcrypt.compare(password, user.password)
 
     if (!isValid) {
-throw new Error('Email or password, is incorrect')
+        throw new Error('Email or password, is incorrect')
     }
 
-   
-const playload = {
-    _id : user._id,
-    email: user.email
-}
 
-const token = await jwt.sign(playload, SECRET, {expiresIn: "2h"})
+    const playload = {
+        _id: user._id,
+        email: user.email
+    }
 
-return token;
+    const token = await jwt.sign(playload, SECRET, { expiresIn: "2h" })
+
+    return token;
 };
